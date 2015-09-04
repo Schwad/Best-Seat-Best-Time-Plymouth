431c7d2330da3c740ea8be7c24e1cdc77ab7bb75
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-09-04 15:22:51 +0100

    includes basic templates for scheduled rake tasks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def updates_page
+    remove_all
+    scrapes
+  end
+
+  def remove_all
+    @old_stories = []
+    @old_scores = []
+    if Story.all.count > 0
+      Story.all.each do |story|
+        @old_stories << story.source
+        if story.points_text != nil
+          @old_scores << story.points_text.split(" ")[0].to_i
+        else
+          puts "problem with #{story.source}"
+        end
+      end
+      Story.destroy_all
+    end
+  end
 end
