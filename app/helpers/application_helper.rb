12e9fe497212035e3ea6ffa648ad8febc5ba1b89
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-09-04 17:26:59 +0100

    fixes script for scraping

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,12 +10,15 @@ module ApplicationHelper
 
     @array_of_rooms = []
 
+    @array_of_total = []
+
     a.search('tr td:nth-child(1)').each do |row|
-      @array_of_rooms = row.text
+        @array_of_rooms << row.text
     end
     a.search('tr td:nth-child(2)').each do |row|
        @array_of_availability << row.text.to_f
     end
+
     a.search('tr td:nth-child(3)').each do |row|
       @array_of_total << row.text.to_f
     end
@@ -23,7 +26,12 @@ module ApplicationHelper
     @array_answer = []
 
     @array_of_availability.each_with_index do |val, index|
+      begin
       @array_answer << (val / @array_of_total[index])
+    rescue
+      puts "problem line 32"
+      binding.pry
+    end
       puts @array_answer[-1]
     end
 
@@ -31,11 +39,11 @@ module ApplicationHelper
 
   def build_data
     @array_of_rooms.each_with_index do |val, index|
-      DataPoint.create(
+      Datapoint.create(
         :free => @array_answer[index],
         :room_name => val
         )
-      puts "val at @array_answer[index]"
+      puts "#{val} at #{@array_answer[index]}"
 
     end
 
