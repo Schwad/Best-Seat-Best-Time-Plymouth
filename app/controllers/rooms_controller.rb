762a3e7a1a0f297176d7c7f6af114ad57f9b10de
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-09-09 09:33:46 +0100

    cleans up code to operate and render faster

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,15 +1,10 @@
 class RoomsController < ApplicationController
   def index
     @rooms =  Datapoint.select("room_name").group("room_name")
-    @room_array = []
-    @thingy = Datapoint.all.sample
-    @rooms.each do |room|
-      @room_array << room.room_name
-    end
+    @thingy = Datapoint.first
   end
 
   def display_props
     @rooms = Datapoint.all
-
   end
 end
