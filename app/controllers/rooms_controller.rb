89e101bb28c0084298b7d0c509c6ea759406b56a
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-19 14:14:10 +0100

    creates model method to return weekly change

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,8 @@ class RoomsController < ApplicationController
   def index
     @rooms =  Datapoint.select("room_name").group("room_name")
     @thingy = Datapoint.first
+    @overall_change = []
+
   end
 
   def display_props
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,16 @@
 class Day < ActiveRecord::Base
   serialize :full_data
+
+
+  def weekly_change
+    room = self.room_name
+    original = Day.where("room_name = ?", room).first
+    total_change = 0
+    original.full_data.each_with_index do |hour, index|
+      difference = self.full_data[index] - hour
+      total_change += difference
+    end
+    full_difference = total_change / 24
+    return total_change
+  end
 end
